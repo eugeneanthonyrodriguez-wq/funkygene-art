b8f988909468433cdb87b15f7a4d861fc4b30cc4
eugeneanthonyrodriguez-wq <eugeneanthonyrodriguez@gmail.com>
2026-08-28 10:42:06 -0700

    Add fake race result data for testing

---
+++ fake_race_result_data.js
@@ -0,0 +1,14 @@
+window.FAKE_RACE_RESULT_DATA = {
+  runners: [
+    { bib: 101, name: "TEST RUNNER ADAM", gender: "M", position: 1, split: "SKYLINE GATE", time: "07:44:22" },
+    { bib: 102, name: "TEST RUNNER DYLAN", gender: "F", position: 2, split: "SKYLINE GATE", time: "07:46:11" },
+    { bib: 103, name: "TEST RUNNER JENNY", gender: "M", position: 3, split: "SKYLINE GATE", time: "07:49:03" },
+    { bib: 104, name: "TEST RUNNER EUGENE", gender: "F", position: 4, split: "SKYLINE GATE", time: "07:52:40" },
+    { bib: 105, name: "TEST RUNNER 05", gender: "M", position: 5, split: "SKYLINE GATE", time: "07:55:18" },
+    { bib: 106, name: "TEST RUNNER 06", gender: "F", position: 6, split: "SKYLINE GATE", time: "07:58:06" },
+    { bib: 107, name: "TEST RUNNER 07", gender: "M", position: 7, split: "SKYLINE GATE", time: "08:01:12" },
+    { bib: 108, name: "TEST RUNNER 08", gender: "F", position: 8, split: "SKYLINE GATE", time: "08:03:44" },
+    { bib: 109, name: "TEST RUNNER 09", gender: "M", position: 9, split: "SKYLINE GATE", time: "08:05:30" },
+    { bib: 110, name: "TEST RUNNER 10", gender: "F", position: 10, split: "SKYLINE GATE", time: "08:08:09" }
+  ]
+};
